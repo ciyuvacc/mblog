e3452621844a518215d66659c4bb37756f864fba
ciyuvacc <850533921@qq.com>
2017-09-22 21:21:08 +0000

    2

--- mblog/settings.py
+++ mblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h+dq%fp5$h4vwlcd2zcz1&dkbg**vdgct9ah_war5$(0^cdqjy'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
